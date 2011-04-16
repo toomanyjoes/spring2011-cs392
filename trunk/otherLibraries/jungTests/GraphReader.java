b88d86a9ac7a496c403096f055ecc63659f3adc0
sdpalme <sdpalme@891bd353-ff1c-4044-f709-d07ffde18174>
2011-04-16 01:45:04 +0000

    Added jung test.

---
+++ trunk/otherLibraries/jungTests/GraphReader.java
@@ -0,0 +1,111 @@
+package jungTests;
+
+import java.io.*;
+import java.util.*;
+import edu.uci.ics.jung.graph.*;
+
+public class GraphReader {
+    private Reader inFile;
+    private boolean directed;
+    private boolean weighted;
+    private String filename;
+    
+    public GraphReader(boolean weighted, String filename) {
+        this.weighted = weighted;
+        this.filename = filename;
+    }
+    
+    public void readGraph(Graph g) {
+        openFile();
+        
+        int numVertices = readNumber();
+        int numEdges = readNumber();
+        readNumber(); 
+        readNumber();
+        readNumber();
+
+        List<Object> vList = new ArrayList<Object>();
+        for (int i = 0; i < numVertices; i++)
+        {
+            Object curr = new Object();
+            vList.add(curr);
+            g.addVertex(curr);
+        }
+
+        List<Object[]> eList = new ArrayList<Object[]>();
+        for(int i=0; i < numEdges; i++)
+        {
+            int start_idx = readNumber();
+            int end_idx = readNumber();
+            Object start = vList.get(start_idx);
+            Object end = vList.get(end_idx);
+            Object[] edge = {start, end};
+            eList.add(edge);
+            if(!weighted)
+                g.addEdge(new MyLink(1), start, end);
+        }
+        
+        if(weighted)
+        {
+            for(int i=0; i < numEdges; i++)
+            {
+                int weight = readNumber();
+                Object[] edge = eList.get(i);
+                g.addEdge(new MyLink(weight), edge[0], edge[1]);
+            }
+        }
+        
+        closeFile();
+    }
+
+    private boolean openFile()
+    {
+        try 
+        {
+            inFile = new FileReader( filename );
+        }
+        catch ( IOException e )
+        {
+            System.out.println( "Your file " + filename + " cannot be read" );
+            return false;
+        }
+        return true;
+    }
+    
+    private void closeFile() {
+        try
+        {
+            inFile.close();
+        }
+        catch(Exception e)
+        { }
+    }
+    
+    private int readNumber()
+    {
+        String theString = null;
+        try {
+        int index = 0;
+        char[ ] word = new char[ 80 ];
+        int ch = 0;
+
+        ch = inFile.read();
+        while( ch==32 )
+        {
+            ch = inFile.read(); // skips extra whitespaces
+        }
+
+        while( ch != -1 && ch != 32 && ch != 10 ) // while it is not EOF, WS, NL
+        {
+            word[ index++ ] = ( char )ch;
+            ch = inFile.read();
+        }
+        word[ index ] = 0;
+
+        theString = new String( word );
+
+        theString = new String( theString.substring( 0,index ) ).trim();
+        } catch(Exception e) { }
+        return Integer.parseInt( theString,10 );
+    }
+}
--- trunk/otherLibraries/runTest.sh
+++ trunk/otherLibraries/runTest.sh
@@ -9,7 +9,9 @@ then
     SEP=";"
 fi
 
-OUTPUT=`java -classpath ".${SEP}jgrapht-0.8.2/build" ${TEST} ${INPUT} | grep "Time elapsed:" | awk '{print $3}'`
+CP=".${SEP}jgrapht-0.8.2/build${SEP}jung2-2_0_1/collections-generic-4.01.jar${SEP}jung2-2_0_1/jung-3d-demos-2.0.1.jar${SEP}jung2-2_0_1/jung-jai-2.0.1.jar${SEP}jung2-2_0_1/vecmath-1.3.1.jar${SEP}jung2-2_0_1/colt-1.2.0.jar${SEP}jung2-2_0_1/jung-algorithms-2.0.1.jar${SEP}jung2-2_0_1/jung-jai-samples-2.0.1.jar${SEP}jung2-2_0_1/wstx-asl-3.2.6.jar${SEP}jung2-2_0_1/concurrent-1.3.4.jar${SEP}jung2-2_0_1/jung-api-2.0.1.jar${SEP}jung2-2_0_1/jung-samples-2.0.1.jar${SEP}jung2-2_0_1/j3d-core-1.3.1.jar${SEP}jung2-2_0_1/jung-graph-impl-2.0.1.jar${SEP}jung2-2_0_1/jung-visualization-2.0.1.jar${SEP}jung2-2_0_1/jung-3d-2.0.1.jar${SEP}jung2-2_0_1/jung-io-2.0.1.jar${SEP}jung2-2_0_1/stax-api-1.0.1.jar"
+
+OUTPUT=`java -classpath ${CP} ${TEST} ${INPUT} | grep "Time elapsed:" | awk '{print $3}'`
 
 
 
